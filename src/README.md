1b1f49322c2e2ec593529b2ae677267c380a3ded
Xheldon <c1006044256@gmail.com>
2020-09-29 00:22:55 +0800

    inputrules complete

--- src/README.md
+++ src/README.md
@@ -2,11 +2,16 @@ This module defines a plugin for attaching _input rules_ to an editor,
 which can react to or transform text typed by the user. It also comes
 with a bunch of default rules that can be enabled in this plugin.
 
+@cn 本模块定义了一个编辑器插件用来附加 _input rules（输入规则）_，它可以响应或者转换用户输入的文字。
+本模块还带有一些默认的规则，可以通过本插件启用。
+
 @InputRule
 @inputRules
 @undoInputRule
 
 The module comes with a number of predefined rules:
+
+@cn 本模块还带有一些预定义的规则：
 
 @emDash
 @ellipsis
@@ -18,6 +23,8 @@ The module comes with a number of predefined rules:
 
 These utility functions take schema-specific parameters and create
 input rules specific to that schema.
+
+@cn 下列这些工具函数接受一个特定于 schema 的参数，并创建一个特定于 schema 的输入规则。
 
 @wrappingInputRule
 @textblockTypeInputRule
--- src/inputrules.js
+++ src/inputrules.js
@@ -4,15 +4,22 @@ import {Plugin} from "prosemirror-state"
 // that, when typed, causes something to happen. This might be
 // changing two dashes into an emdash, wrapping a paragraph starting
 // with `"> "` into a blockquote, or something entirely different.
+//
+// @cn 输入规则是一些正则表达式，描述了输入何种文本会引起一些额外的变化。这个变化可能是将两个短斜杠变成一个长破折号，或者将以 `"> "` 开头的段落用 blockquote 包裹着，
+// 亦或者其他完全不一样的事情。
 export class InputRule {
   // :: (RegExp, union<string, (state: EditorState, match: [string], start: number, end: number) → ?Transaction>)
   // Create an input rule. The rule applies when the user typed
   // something and the text directly in front of the cursor matches
   // `match`, which should probably end with `$`.
   //
+  // @cn 创建一个输入规则。规则会应用到当用户输入一些内容的时候，且直接在光标之前的文本会匹配 `match` 参数，该参数应该合适的用 `$` 结尾。
+  //
   // The `handler` can be a string, in which case the matched text, or
   // the first matched group in the regexp, is replaced by that
   // string.
+  //
+  // @cn `handler` 参数可以是一个字符串，这种情况下表示匹配的文本，或者在正则中匹配的第一个组，会被该字符串替换。
   //
   // Or a it can be a function, which will be called with the match
   // array produced by
@@ -20,6 +27,9 @@ export class InputRule {
   // as well as the start and end of the matched range, and which can
   // return a [transaction](#state.Transaction) that describes the
   // rule's effect, or null to indicate the input was not handled.
+  //
+  // @cn 它也可以是一个函数，会将调用 [`RegExp.exec`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec) 后产生的
+  // 结果匹配数组传入作为参数来调用，以及匹配的起始和结束的范围。函数返回一个描述了规则影响的 [transaction](#state.Transaction)，或者如果输入没有被处理则返回 null。
   constructor(match, handler) {
     this.match = match
     this.handler = typeof handler == "string" ? stringHandler(handler) : handler
@@ -49,6 +59,8 @@ const MAX_MATCH = 500
 // Create an input rules plugin. When enabled, it will cause text
 // input that matches any of the given rules to trigger the rule's
 // action.
+//
+// @cn 创建一个输入规则插件。启用的话，将会导致与任何给定规则匹配的文本输入都会触发该规则对应的行为。
 export function inputRules({rules}) {
   let plugin = new Plugin({
     state: {
@@ -98,6 +110,8 @@ function run(view, from, to, text, rules, plugin) {
 // :: (EditorState, ?(Transaction)) → bool
 // This is a command that will undo an input rule, if applying such a
 // rule was the last thing that the user did.
+//
+// @cn 如果应用这个规则是用户做的最后一件事情的话，这是一个可以撤销输入规则的命令。
 export function undoInputRule(state, dispatch) {
   let plugins = state.plugins
   for (let i = 0; i < plugins.length; i++) {
