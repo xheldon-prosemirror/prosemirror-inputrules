bc67be71489a732220ba5d93b7f12ac246776644
Xheldon <c1006044256@gmail.com>
2020-09-29 11:11:51 +0800

    inputrules real-complete

--- src/rulebuilders.js
+++ src/rulebuilders.js
@@ -8,15 +8,25 @@ import {findWrapping, canJoin} from "prosemirror-transform"
 // probably want the regexp to start with `^`, so that the pattern can
 // only occur at the start of a textblock.
 //
+// @cn 当给定字符串被输入的时候构建一个输入规则以自动包裹一个文本块。`regexp` 参数被直接传给 `InputRule` 构造函数。你也许想要正则表达式以 `^` 开头，
+// 这样的话就只会从一个文本块起始位置开始匹配。
+//
+// @comment `^` 表示正则中的起始位置匹配，一般用来做类似于 markdown 的输入规则，例如在文本块开头输入 # + 空格后，生成一个 h 元素。
+//
 // `nodeType` is the type of node to wrap in. If it needs attributes,
 // you can either pass them directly, or pass a function that will
 // compute them from the regular expression match.
+//
+// @cn `nodeType` 是要被包裹进的节点类型。如果它需要 attributes，那么你既可以直接传入，也可以传入一个计算 attributes 的函数，该函数接受正则匹配的结果作为参数。
 //
 // By default, if there's a node with the same type above the newly
 // wrapped node, the rule will try to [join](#transform.Transform.join) those
 // two nodes. You can pass a join predicate, which takes a regular
 // expression match and the node before the wrapped node, and can
 // return a boolean to indicate whether a join should happen.
+//
+// @cn 默认情况下，如果有新的包裹节点之前有一个与之相同类型的节点，那么这个规则将会尝试 [join（连接）](#transform.Transform.join) 这两个节点。
+// 你可以传递一个连接指示函数，它接受一个正则表达式的结果和在包裹节点之前的节点作为参数，返回一个指示连接是否应该进行的布尔值。
 export function wrappingInputRule(regexp, nodeType, getAttrs, joinPredicate) {
   return new InputRule(regexp, (state, match, start, end) => {
     let attrs = getAttrs instanceof Function ? getAttrs(match) : getAttrs
@@ -39,6 +49,9 @@ export function wrappingInputRule(regexp, nodeType, getAttrs, joinPredicate) {
 // textblock. The optional `getAttrs` parameter can be used to compute
 // the new node's attributes, and works the same as in the
 // `wrappingInputRule` function.
+//
+// @cn 构建一个输入规则，以当匹配的文本输入的时候能够改变文本块的类型。你的正则通常应该以 `^` 开头，这样它就会只匹配文本块的起始位置。
+// 可选参数 `getAttrs` 可以被用来计算新节点的 attributes，功能和 `wrappingInputRule` 中的该函数一样。
 export function textblockTypeInputRule(regexp, nodeType, getAttrs) {
   return new InputRule(regexp, (state, match, start, end) => {
     let $start = state.doc.resolve(start)
--- src/rules.js
+++ src/rules.js
@@ -1,17 +1,31 @@
 import {InputRule} from "./inputrules"
 
 // :: InputRule Converts double dashes to an emdash.
+//
+// @cn 转换两个短斜杠为一个长破折号的输入规则。
 export const emDash = new InputRule(/--$/, "—")
 // :: InputRule Converts three dots to an ellipsis character.
+//
+// @cn 转换三个点为一个省略号的输入规则。
 export const ellipsis = new InputRule(/\.\.\.$/, "…")
 // :: InputRule “Smart” opening double quotes.
+//
+// @cn 「智能」打开双引号的输入规则。
 export const openDoubleQuote = new InputRule(/(?:^|[\s\{\[\(\<'"\u2018\u201C])(")$/, "“")
 // :: InputRule “Smart” closing double quotes.
+//
+// @cn 「智能」关闭双引号的输入规则。
 export const closeDoubleQuote = new InputRule(/"$/, "”")
 // :: InputRule “Smart” opening single quotes.
+//
+// @cn 「智能」打开单引号的输入规则。
 export const openSingleQuote = new InputRule(/(?:^|[\s\{\[\(\<'"\u2018\u201C])(')$/, "‘")
 // :: InputRule “Smart” closing single quotes.
+//
+// @cn 「智能」关闭单引号的输入规则。
 export const closeSingleQuote = new InputRule(/'$/, "’")
 
 // :: [InputRule] Smart-quote related input rules.
+//
+// @cn 自动打开/关闭 单/双 引号相关的输入规则。
 export const smartQuotes = [openDoubleQuote, closeDoubleQuote, openSingleQuote, closeSingleQuote]
